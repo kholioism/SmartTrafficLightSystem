3233d90a8836b7c10c148b4ecc049780043baa3b
kholioism <khalidshehab04@gmail.com>
2024-12-02 01:48:26 +0200

    Temperature Sensor Description

--- sensors.vhd
+++ sensors.vhd
@@ -5,7 +5,9 @@ use ieee.std_logic_unsigned.all;
 
 entity sensors is
   port (
-    temp: in std_logic_vector(39 downto 0);
+    temp: in std_logic_vector(39 downto 0); --8bit humidity integer data + 8bit the Humidity decimal data 
+                                            --+8 bit temperature integer data + 8bit fractional temperature data 
+                                            --+8 bit parity bit
     ultra_in: in std_logic_vector(3 downto 0);
     ultra_out: out std_logic_vector(3 downto 0)
   ) ;
